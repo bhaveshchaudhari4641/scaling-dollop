72d2cd69f79cf73eb66bdd931285731dd0c188c7
bhaveshchaudhari4641 <bhaveshchaudhari4641@gmail.com>
2026-02-13 12:01:25 +0530

    Update Assignment 1 rupees_to_dollars.py

--- Assignment 1 rupees_to_dollars.py
+++ Assignment 1 rupees_to_dollars.py
@@ -1,7 +1,5 @@
 salary_in_rs = float(input("Enter the salary in rupees : "))
 
-salary_in_dollars = salary_in_rs / 90.73
+salary_in_dollars = salary_in_rs / 90.12
 
 print("Salary in dollors is $: ", salary_in_dollars)
-
-#PRN: 202501080224
